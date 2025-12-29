66953ebb38b9ee239d3921592f706cda3c8409b8
Monty <wm_8800@163.com>
2025-12-29 18:15:29 +0800

    忽略 OpenNext 构建产物

--- .open-next/server-functions/default/.next/server/chunks/ssr/_next-internal_server_app_test-data_page_actions_3648ceb4.js
+++
@@ -1,3 +0,0 @@
-module.exports=[20326,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_test-data_page_actions_3648ceb4.js.map
--- .open-next/server-functions/default/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"lcZI2JPPeB7b++RAzQRrmIwdJXgiX+yoFjLOLeB6zCA=\"\n}"
